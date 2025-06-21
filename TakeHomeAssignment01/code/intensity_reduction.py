9d5119355c452e302d23af58abcdf35d2610912e
tishanW98 <shakilatishan1998@outlook.com>
2025-06-21 13:32:59 +0530

    intensity reduction code added

---
+++ TakeHomeAssignment01/code/intensity_reduction.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import numpy as np
+
+
+def reduce_intensity(image_path, levels):
+    image = Image.open(image_path).convert('L')  # Grayscale
+    img_np = np.array(image)
+
+    step = 256 // levels
+    reduced = (img_np // step) * step
+    Image.fromarray(reduced.astype(np.uint8)).save(
+        f'../output/intensity/intensity_{levels}.png')
+    return reduced
+
+
+reduced_img = reduce_intensity('../input/girl.jpg', 2)

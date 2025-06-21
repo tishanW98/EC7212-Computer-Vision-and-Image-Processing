13caadb58729fbfa62c061dbef5c71a8f909e319
tishanW98 <shakilatishan1998@outlook.com>
2025-06-21 13:47:59 +0530

    spatial average task code updated

---
+++ TakeHomeAssignment01/code/rotation.py
@@ -0,0 +1,11 @@
+from PIL import Image
+
+
+def rotate_image(image_path, angles):
+    image = Image.open(image_path)
+    for angle in angles:
+        rotated = image.rotate(angle, expand=True)
+        rotated.save(f'../output/rotation/rotated_{angle}.png')
+
+
+rotate_image('../input/girl.jpg', [45, 90])
